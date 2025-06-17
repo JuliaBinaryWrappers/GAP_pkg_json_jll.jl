e97ca8193ea6938291c288fd3ae8c70c91c81472
jlbuild <juliabuildbot@gmail.com>
2025-06-17 16:10:55 +0000

    GAP_pkg_json_jll build 200.200.201+0

--- src/wrappers/powerpc64le-linux-gnu-cxx03.jl
+++ src/wrappers/powerpc64le-linux-gnu-cxx03.jl
@@ -2,11 +2,10 @@
 export json
 
 using GAP_jll
-using GAP_lib_jll
 JLLWrappers.@generate_wrapper_header("GAP_pkg_json")
 JLLWrappers.@declare_file_product(json)
 function __init__()
-    JLLWrappers.@generate_init_header(GAP_jll, GAP_lib_jll)
+    JLLWrappers.@generate_init_header(GAP_jll)
     JLLWrappers.@init_file_product(
         json,
         "lib/gap/json.so",
